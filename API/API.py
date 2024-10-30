7bd7a9d47e406795056eb720370779898cc7d8e5
AlphaTango-ops <tabele17@gmail.com>
2024-10-29 20:25:03 -0400

    did some m ore work with the api and will soon be at a good stop to do some tensorflow work

--- API/API.py
+++ API/API.py
@@ -1,9 +1,11 @@
-import matplotlib
+
 import pandas as pd 
 import numpy as np 
-import seaborn as sns 
 import matplotlib.pyplot as plt
 import yfinance as yf
 
+# format for setting up any stock: aapl = yf.Ticker("AAPL")
 aapl = yf.Ticker("AAPL")
-
+aapl_history = aapl.history(period ="max")
+aapl_history.reset_index(inplace=True)
+df = aapl_history[['Date','Close']]
