ca182357f6f27ca4ffb1e02940b5b3f5fd94c743
Landon Burtle <landonburtle10@gmail.com>
2024-11-04 19:19:30 -0500

    MIGRATE NONSENSE

--- Imported_DAO_script/starter-template/daostack-migrate.sh
+++ Imported_DAO_script/starter-template/daostack-migrate.sh
@@ -1,1 +1,2 @@
-daostack-migrate dao --params /Users/landonburtle/2024_Fall/Datascience/DAO/Imported_DAO_script/starter-template/data/testDaoSpec.json --provider https://eth-sepolia.g.alchemy.com/v2/0_x1OKjfKi8QkN8eG76NX_Qeem_C0pZu --private-key e4a71933926b5b9cc6dd5a3123b61f1f9be553b8861df8ab34b249d9499df09a
+daostack-migrate dao --params /Users/landonburtle/2024_Fall/Datascience/DAO/Imported_DAO_script/starter-template/data/testDaoSpec.json --provider https://eth-sepolia.g.alchemy.com/v2/0_x1OKjfKi8QkN8eG76NX_Qeem_C0pZu --private-key 8fdfdb536318a2fff6d9473997431923a34141e9eb2e1a32ae472737e1a14460
+
